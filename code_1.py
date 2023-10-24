38991caf38aaf960e2a1c444d71d3e9a6700e02a
traxylund <143898484+traxylund@users.noreply.github.com>
2023-10-24 03:44:16 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,17 @@
-    main()
-    for item in data:
+        console.log("Random Number: " + item);
 
-if __name__ == "__main__":
 
-def generate_random_data():
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    return data;
+main();
+    data.forEach(item => {
+    const data = [];
+    }
+    });
+}
+function main() {
 
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-    return data
-
-        print(f"Random Number: {item}")
-import random
+function generateRandomData() {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
