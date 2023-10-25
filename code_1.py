4362f2fbd2f92c4ecc0a7f87385da0cf72b143fc
traxylund <143898484+traxylund@users.noreply.github.com>
2023-10-25 04:51:42 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,39 @@
-        console.log("Random Number: " + item);
 
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
 
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-    return data;
-main();
-    data.forEach(item => {
-    const data = [];
     }
-    });
+        }
+        for (int item : data) {
+    public static void main(String[] args) {
 }
-function main() {
+            System.out.println("Random Number: " + item);
+    return data
+import random
+    main()
 
-function generateRandomData() {
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+if __name__ == "__main__":
+def generate_random_data():if __name__ == "__main__":
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+
+
+def main():
+def generate_random_data():
+import random
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
