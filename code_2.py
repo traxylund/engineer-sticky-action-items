ae5318f86d4a924ac7f1291a7cdbd7bb1f6a174c
traxylund <143898484+traxylund@users.noreply.github.com>
2023-11-02 12:59:06 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,41 +1,14 @@
-    public static void main(String[] args) {
 
-        int[] data = new int[10];
-    }
+def main():
+    for item in data:
+    return data
 
-        }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-}
-        for (int item : data) {}
-function main() {
-function generateRandomData() {
+    main()
 
-        console.log("Random Number: " + item);
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
 
-    const data = generateRandomData();
-
-    }
-    });
-    data.forEach(item => {
-    const data = [];
-}
-main();
-    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {    }
-        }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-        }
-}
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-
-public class RandomDataGenerator {
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
