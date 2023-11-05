e0532afd8b6299652c4873077eb88a95f5ecab5f
traxylund <143898484+traxylund@users.noreply.github.com>
2023-11-05 16:24:08 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,13 @@
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-}
-    public static void main(String[] args) {
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        }
 
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
 
-        int[] data = new int[10];
+    return data
+if __name__ == "__main__":
+    main()
+    data = generate_random_data()
+def main():
+
+    for item in data:
+def generate_random_data():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
